93d32e083e80d5835e46bd59385411da37b30627
RJvanBreda <rjvanbreda@yahoo.com>
2020-11-12 19:22:42 +0200

    update

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,11 @@ const User = require('../models/user')
 //all users
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.name !=null && req.query.name !== '') {
+    if (req.query.name !=null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try{
-        const users = await User.find({searchOptions})
+        const users = await User.find(searchOptions)
         res.render('users/index', {
             users: users, 
             searchOptions: req.query})
